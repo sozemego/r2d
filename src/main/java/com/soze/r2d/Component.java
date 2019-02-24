ce221601248d2f5e7ebc995a8455e49a19661477
--global <piknypiec@gmail.com>
2019-02-24 01:16:39 +0100

    Add more overloaded methods for setState

--- src/main/java/com/soze/r2d/Component.java
+++ src/main/java/com/soze/r2d/Component.java
@@ -16,6 +16,23 @@ public abstract class Component {
     R.getStateUpdater().accept(() -> stateChangeCallback.run());
   }
 
+  public void setState(String key, Object value) {
+    UiState nextState = new UiState();
+    nextState.set(key, value);
+    setState(nextState);
+  }
+
+  public void setState(Object[] ...pairs) {
+    UiState nextState = new UiState();
+    for (int i = 0; i < pairs.length; i++) {
+      Object[] pair = pairs[i];
+      String key = (String) pair[0];
+      Object value = pair[1];
+      nextState.set(key, value);
+    }
+    setState(nextState);
+  }
+
   public UiState getProps() {
     return props;
   }
